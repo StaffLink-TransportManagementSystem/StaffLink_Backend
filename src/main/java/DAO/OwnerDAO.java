95013a9bba95c6005dd88ea08dde3f72308c1232
Ravindu Hasanka <rhatu2000@gmail.com>
2023-10-04 14:56:02 +0530

    OwnerDAO createowner added

--- src/main/java/DAO/OwnerDAO.java
+++ src/main/java/DAO/OwnerDAO.java
@@ -13,6 +13,40 @@ public class OwnerDAO {
 
         return 0;
     }
-    
+    public static OwnerModel createOwner(OwnerModel owner){
+        Connection connection = (Connection) DBConnection.getInstance();
+        Connection con = null;
+//        OwnerModel owner = new OwnerModel();
+        try{
+            con = connection;
+            String sql = "INSERT INTO owners (name,email,NIC,address,contactNo,income,balance,password) VALUES (?,?,?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1,owner.getName());
+            preparedStatement.setString(2,owner.getEmail());
+            preparedStatement.setString(3,owner.getNIC());
+            preparedStatement.setString(4,owner.getAddress());
+            preparedStatement.setString(5,owner.getContactNo());
+            preparedStatement.setFloat(6,owner.getIncome());
+            preparedStatement.setFloat(7,owner.getBalance());
+            preparedStatement.setString(8,owner.getPassword());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if(resultSet.next()){
+                owner.setId(resultSet.getInt(1));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (con != null) try {
+                con.close();
+            } catch (Exception ignore) {
+            }
+        }
+
+        return owner;
+    }
 
 }
