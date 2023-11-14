04713cc1e56fce122243692e929bb1018b41ba19
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-14 11:57:38 +0530

    route model created

--- src/main/java/Model/route.java
+++ src/main/java/Model/route.java
@@ -1,4 +1,67 @@
 package Model;
 
 public class route {
+    private String routeNo;
+    private String vehicleNo;
+    private String style;
+    private String staringLocation;
+    private String endingLocation;
+    private String startingTime;
+    private String endingTime;
+
+    public String getRouteNo() {
+        return routeNo;
+    }
+
+    public void setRouteNo(String routeNo) {
+        this.routeNo = routeNo;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getStaringLocation() {
+        return staringLocation;
+    }
+
+    public void setStaringLocation(String staringLocation) {
+        this.staringLocation = staringLocation;
+    }
+
+    public String getEndingLocation() {
+        return endingLocation;
+    }
+
+    public void setEndingLocation(String endingLocation) {
+        this.endingLocation = endingLocation;
+    }
+
+    public String getStartingTime() {
+        return startingTime;
+    }
+
+    public void setStartingTime(String startingTime) {
+        this.startingTime = startingTime;
+    }
+
+    public String getEndingTime() {
+        return endingTime;
+    }
+
+    public void setEndingTime(String endingTime) {
+        this.endingTime = endingTime;
+    }
 }
