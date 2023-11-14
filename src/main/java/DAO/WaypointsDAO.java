d289f0bfd2608c92afc5d2d10bb7162f1f9a0bf3
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-14 12:25:07 +0530

    waypoint DAO done

--- src/main/java/DAO/WaypointsDAO.java
+++ src/main/java/DAO/WaypointsDAO.java
@@ -2,7 +2,12 @@ package DAO;
 import Database.DBConnection;
 import Model.Waypoints;
 
+import java.sql.Array;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WaypointsDAO {
     public static boolean addWaypoint(Waypoints waypoints) {
@@ -11,7 +16,7 @@ public class WaypointsDAO {
         boolean success = false;
         try {
             String sql = "INSERT INTO waypoints (waypointId,routeNo,location,orderNo) VALUES (?,?,?,?)";
-            java.sql.PreparedStatement preparedStatement = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, waypoints.getWaypointId());
             preparedStatement.setString(2, waypoints.getRouteNo());
             preparedStatement.setString(3, waypoints.getLocation());
@@ -22,5 +27,64 @@ public class WaypointsDAO {
             System.out.println(e);
         }
         return success;
+    }
+
+    public static boolean deleteWaypoint(Waypoints waypoints) {
+        System.out.println("Inside deleteWaypointDAO");
+        Connection connection = DBConnection.getInstance().getConnection();
+        boolean success = false;
+        try {
+            String sql = "DELETE FROM waypoints WHERE waypointId = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, waypoints.getWaypointId());
+            preparedStatement.executeUpdate();
+            success = true;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return success;
+    }
+
+    public static boolean updateWaypoint(Waypoints waypoints) {
+        System.out.println("Inside updateWaypointDAO");
+        Connection connection = DBConnection.getInstance().getConnection();
+        boolean success = false;
+        try {
+            String sql = "UPDATE waypoints SET routeNo = ?, location = ?, orderNo = ? WHERE waypointId = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, waypoints.getRouteNo());
+            preparedStatement.setString(2, waypoints.getLocation());
+            preparedStatement.setInt(3, waypoints.getOrderNo());
+            preparedStatement.setString(4, waypoints.getWaypointId());
+            preparedStatement.executeUpdate();
+            success = true;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return success;
+    }
+
+    public static List<Waypoints> getwaypoint(String routeNo) {
+        System.out.println("Inside getwaypointDAO");
+        Connection connection = DBConnection.getInstance().getConnection();
+        List<Waypoints> waypoints = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM waypoints WHERE routeNo = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, routeNo);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Waypoints waypoint = new Waypoints();
+                waypoint.setWaypointId(resultSet.getString("waypointId"));
+                waypoint.setRouteNo(resultSet.getString("routeNo"));
+                waypoint.setLocation(resultSet.getString("location"));
+                waypoint.setOrderNo(resultSet.getInt("orderNo"));
+                waypoints.add(waypoint);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return waypoints;
     }
 }
