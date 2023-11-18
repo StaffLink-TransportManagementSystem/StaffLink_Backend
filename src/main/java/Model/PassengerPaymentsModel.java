18bbd5704f0124b9fc2ca872ea88e9d410e37e6c
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-19 01:11:15 +0530

    passenger payments model done

--- src/main/java/Model/PassengerPaymentsModel.java
+++ src/main/java/Model/PassengerPaymentsModel.java
@@ -1,4 +1,67 @@
 package Model;
 
 public class PassengerPaymentsModel {
+    int id;
+    String passengerEmail;
+    String vehicleNo;
+    String date;
+    String paymentType; //card or cash
+    float amount;
+
+    public PassengerPaymentsModel() {
+    }
+    public PassengerPaymentsModel(String passengerEmail, String vehicleNo, String date, float amount) {
+        this.passengerEmail = passengerEmail;
+        this.vehicleNo = vehicleNo;
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassengerEmail() {
+        return passengerEmail;
+    }
+
+    public void setPassengerEmail(String passengerEmail) {
+        this.passengerEmail = passengerEmail;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
 }
