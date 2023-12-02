c12678e3fca3fa29e9f2cc48732ff3f9e51c3003
Ravindu Hasanka <rhatu2000@gmail.com>
2023-12-02 16:26:15 +0530

    Reservation model created

---
+++ src/main/java/Model/ReservationModel.java
@@ -0,0 +1,92 @@
+package Model;
+
+public class ReservationModel {
+    private int reservationId;
+    private String passengerEmail;
+    private String vehicleNo;
+    private String startingDate;
+    private String endingDate;
+    private int wayPointId;
+
+    public ReservationModel() {
+    }
+    public ReservationModel(String passengerEmail, String vehicleNo, String startingDate, String endingDate) {
+        this.passengerEmail = passengerEmail;
+        this.vehicleNo = vehicleNo;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+    }
+    public ReservationModel(String passengerEmail, String vehicleNo, String startingDate, String endingDate, int wayPointId) {
+        this.passengerEmail = passengerEmail;
+        this.vehicleNo = vehicleNo;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+        this.wayPointId = wayPointId;
+    }
+    public ReservationModel(int reservationId, String passengerEmail, String vehicleNo, String startingDate, String endingDate) {
+        this.reservationId = reservationId;
+        this.passengerEmail = passengerEmail;
+        this.vehicleNo = vehicleNo;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+    }
+    public ReservationModel(int reservationId, String passengerEmail, String vehicleNo, String startingDate, String endingDate, int wayPointId) {
+        this.reservationId = reservationId;
+        this.passengerEmail = passengerEmail;
+        this.vehicleNo = vehicleNo;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+        this.wayPointId = wayPointId;
+    }
+    public ReservationModel(int reservationId){
+        this.reservationId = reservationId;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public String getPassengerEmail() {
+        return passengerEmail;
+    }
+
+    public void setPassengerEmail(String passengerEmail) {
+        this.passengerEmail = passengerEmail;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(String startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public String getEndingDate() {
+        return endingDate;
+    }
+
+    public void setEndingDate(String endingDate) {
+        this.endingDate = endingDate;
+    }
+
+    public int getWayPointId() {
+        return wayPointId;
+    }
+
+    public void setWayPointId(int wayPointId) {
+        this.wayPointId = wayPointId;
+    }
+}
