baf7e31cccc3eda62ba9bb5ca9201c096cacd2c6
Ravindu Hasanka <rhatu2000@gmail.com>
2023-12-18 10:13:05 +0530

    passenger validations enhanced

--- src/main/java/Validation/Passenger.java
+++ src/main/java/Validation/Passenger.java
@@ -16,13 +16,7 @@ public class Passenger {
     public static boolean validateEmail(String email) {
         String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         Pattern pattern = Pattern.compile(EMAIL_REGEX);
-        if (email.length() > 0) {
-            return true;
-        }
-        else if(pattern.matcher(email).matches()){
-            return true;
-        }
-        else if(!emailExists(email)){
+        if ((email.length() > 0) && (pattern.matcher(email).matches()) && (!emailExists(email))) {
             return true;
         }
         else {
@@ -40,13 +34,7 @@ public class Passenger {
     private static boolean validateNIC(String NIC) {
         String NIC_REGEX = "^(\\d{9}[vVxX]|\\d{12})$";
         Pattern pattern = Pattern.compile(NIC_REGEX);
-        if (NIC.length() > 0) {
-            return true;
-        }
-        else if(pattern.matcher(NIC).matches()){
-            return true;
-        }
-        else if(!NICExists(NIC)){
+        if ((NIC.length() > 0) && pattern.matcher(NIC).matches() && (!NICExists(NIC))) {
             return true;
         }
         else {
@@ -64,9 +52,7 @@ public class Passenger {
     private static boolean validateContact(String contact) {
         String CONTACT_NUMBER_REGEX = "^(\\+\\d{2})?\\d{10}$";
         Pattern pattern = Pattern.compile(CONTACT_NUMBER_REGEX);
-        if (contact.length() > 0) {
-            return true;
-        } else if(pattern.matcher(contact).matches()){
+        if ((contact.length() == 10 || contact.length() == 12) && pattern.matcher(contact).matches()){
             return true;
         }
         else {
@@ -77,9 +63,7 @@ public class Passenger {
         String PASSWORD_REGEX ="^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
         Pattern pattern = Pattern.compile(PASSWORD_REGEX);
 
-        if (password.length() > 0) {
-            return true;
-        } else if(pattern.matcher(password).matches()){
+        if ((password.length() > 0) && (pattern.matcher(password).matches())) {
             return true;
         }
         else {
@@ -94,10 +78,12 @@ public class Passenger {
             return false;
         }
     }
-    public static PassengerModel setPassenger(PassengerModel passenger) {
+
+    public static boolean validationOnUpdate(PassengerModel passenger) {
         PassengerModel passengerModel = new PassengerModel();
         PassengerDAO passengerDAO = new PassengerDAO();
         passengerModel = passengerDAO.getPassenger(passenger.getEmail());
+        boolean valid = true;
         if(passenger.getId() == 0) {
             passenger.setId(passengerModel.getId());
         }
@@ -110,7 +96,42 @@ public class Passenger {
         if(passenger.getPassword()== null) {
             passenger.setPassword(passengerModel.getPassword());
         }
-        return passenger;
+        if(passenger.getContactNo() == null) {
+            passenger.setContactNo(passengerModel.getContactNo());
+        }
+        valid = validation(passenger);
+        if(valid) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static PassengerModel setPassenger(PassengerModel passenger) {
+        PassengerModel passengerModel = new PassengerModel();
+        PassengerDAO passengerDAO = new PassengerDAO();
+        passengerModel = passengerDAO.getPassenger(passenger.getEmail());
+        boolean valid = true;
+        if(passenger.getId() == 0) {
+            passenger.setId(passengerModel.getId());
+        }
+        if(passenger.getName() == null) {
+            passenger.setName(passengerModel.getName());
+        }
+        if(passenger.getNIC() == null) {
+            passenger.setNIC(passengerModel.getNIC());
+        }
+        if(passenger.getPassword()== null) {
+            passenger.setPassword(passengerModel.getPassword());
+        }
+        if(passenger.getContactNo() == null) {
+            passenger.setContactNo(passengerModel.getContactNo());
+        }
+        valid = validationOnUpdate(passenger);
+        if(valid) {
+            return passenger;
+        } else {
+            return null;
+        }
     }
 
     public Passenger() {}
