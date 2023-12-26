1f6703aa934bad385912a761a9e8bdfbc5599e35
Ravindu Hasanka <rhatu2000@gmail.com>
2023-12-26 23:42:40 +0530

    Get image testing success

--- src/main/java/Controller/testSendImages.java
+++ src/main/java/Controller/testSendImages.java
@@ -14,38 +14,82 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
+
+//@WebServlet("/testSendImages")
+//public class testSendImages extends HttpServlet {
+//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("application/json");
+//        PrintWriter out = response.getWriter();
+//        System.out.println("Image Sending servelet");
+//
+//        try {
+//            Gson gson = new Gson();
+//            BufferedReader bufferedReader = request.getReader();
+//            VehicleModel vehicle = gson.fromJson(bufferedReader, VehicleModel.class);
+//
+////            VehicleImageDAO vehicleImageDAO = new VehicleImageDAO();
+////            List<VehicleModel> vehicleImages = vehicleImageDAO.getVehicleImages(vehicle.getVehicleNo());
+//
+////            byte[] image = convertImageToByteArray("C:/Users/User/Documents/Group Project/Implementation/try2/src/main/java/Images"+vehicleImages.get(0).getFrontImage()+"."+vehicleImages.get(0).getFrontImageType());
+//            byte[] image = convertImageToByteArray("C:/Users/User/Documents/Group Project/Implementation/try2/src/main/java/Images/Capture.png");
+//
+//            if (image.length != 0) {
+//                response.setStatus(HttpServletResponse.SC_OK);
+//                out.write("{\"size\": " + image.length + ",\"list\":" + image + "}");
+//                System.out.println("View all images");
+//            } else if (image.length == 0) {
+//                response.setStatus(HttpServletResponse.SC_ACCEPTED);
+//                out.write("{\"size\": \"0\"}");
+//                System.out.println("No images");
+//            }
+//
+//
+//
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        } finally {
+//            out.close();
+//        }
+//    }
+//
+//    private byte[] convertImageToByteArray(String imagePath) throws IOException {
+//        Path path = Paths.get(imagePath);
+//        return Files.readAllBytes(path);
+//    }
+//}
+
 
 @WebServlet("/testSendImages")
 public class testSendImages extends HttpServlet {
+
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
-        System.out.println("Image uploading servelet");
+        System.out.println("Image Sending servlet");
 
         try {
             Gson gson = new Gson();
             BufferedReader bufferedReader = request.getReader();
             VehicleModel vehicle = gson.fromJson(bufferedReader, VehicleModel.class);
 
-//            VehicleImageDAO vehicleImageDAO = new VehicleImageDAO();
-//            List<VehicleModel> vehicleImages = vehicleImageDAO.getVehicleImages(vehicle.getVehicleNo());
-
-//            byte[] image = convertImageToByteArray("C:/Users/User/Documents/Group Project/Implementation/try2/src/main/java/Images"+vehicleImages.get(0).getFrontImage()+"."+vehicleImages.get(0).getFrontImageType());
             byte[] image = convertImageToByteArray("C:/Users/User/Documents/Group Project/Implementation/try2/src/main/java/Images/Capture.png");
 
             if (image.length != 0) {
+                // Convert the byte array to a Base64-encoded string
+                String base64Image = Base64.getEncoder().encodeToString(image);
+
                 response.setStatus(HttpServletResponse.SC_OK);
-                out.write("{\"size\": " + image.length + ",\"list\":" + image + "}");
+                out.write("{\"size\": " + image.length + ", \"base64Image\": \"" + base64Image + "\"}");
                 System.out.println("View all images");
-            } else if (image.length == 0) {
+            } else {
                 response.setStatus(HttpServletResponse.SC_ACCEPTED);
                 out.write("{\"size\": \"0\"}");
                 System.out.println("No images");
             }
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
