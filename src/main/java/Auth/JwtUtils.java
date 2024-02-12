cf5b747ab57df0dda794466c4e85b58689725de4
Ravindu Hasanka <rhatu2000@gmail.com>
2024-02-12 10:23:54 +0530

    Owner JWT generation

--- src/main/java/Auth/JwtUtils.java
+++ src/main/java/Auth/JwtUtils.java
@@ -69,4 +69,6 @@ public class JwtUtils {
             throw new RuntimeException();
         }
     }
+
+
 }
--- src/main/java/Controller/loginOwner.java
+++ src/main/java/Controller/loginOwner.java
@@ -1,10 +1,12 @@
 package Controller;
 
 
+import Auth.JwtUtils;
 import Model.OwnerModel;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +18,7 @@ import java.util.regex.Pattern;
 
 import Model.loginModel;
 import com.google.gson.Gson;
+import org.json.JSONObject;
 
 @WebServlet("/ownerLogin")
 public class loginOwner extends HttpServlet{
@@ -28,28 +31,64 @@ public class loginOwner extends HttpServlet{
 
             // json data to user object
             BufferedReader bufferedReader = req.getReader();
-            loginModel owner = gson.fromJson(bufferedReader, loginModel.class);
-            OwnerModel ownerModel = owner.getOwner();
-            System.out.println(ownerModel.getEmail());
-            System.out.println(ownerModel.getPassword());
+            loginModel loginModel = gson.fromJson(bufferedReader, loginModel.class);
+            OwnerModel ownerModel = loginModel.getOwner();
             System.out.println(ownerModel.getEmail());
             System.out.println(ownerModel.getPassword());
 
-            // All validations are passed then register
+
             if(ownerModel.getId() != 0){
                 res.setStatus(HttpServletResponse.SC_OK);
-                if(ownerModel.getPassword().equals(owner.getPassword())) {
+                if(ownerModel.getPassword().equals(loginModel.getPassword())) {
+
+                    JSONObject payload = new JSONObject();
+                    payload.put("email", ownerModel.getEmail());
+                    payload.put("id", ownerModel.getEmail());
+                    payload.put("page", "owner");
+
+                    JwtUtils jwtUtils = new JwtUtils(payload);
+                    String token = jwtUtils.generateJwt();
+
+                    Cookie cookie = new Cookie("jwt", token);
+                    cookie.setPath("/");
+                    cookie.setSecure(true); // For HTTPS
+                    cookie.setHttpOnly(false);
+
+                    // Set the cookie to expire after one day (in seconds)
+                    int oneDayInSeconds = 24 * 60 * 60;
+                    cookie.setMaxAge(oneDayInSeconds);
+
+                    res.addCookie(cookie);
+
+                    res.setStatus(HttpServletResponse.SC_OK);
                     out.write("{\"message\": \"Login successfully\",\"page\":\""+ "owner" +"\"}");
                     System.out.println("Login successful");
+
                 }else{
                     out.write("{\"message\": \"Wrong Password\"}");
                     System.out.println("Wrong password");
                 }
             }else{
-                res.setStatus(HttpServletResponse.SC_OK);
+                res.setStatus(HttpServletResponse.SC_ACCEPTED);
                 out.write("{\"message\": \"Invalid Email\"}");
                 System.out.println("Login incorrect");
             }
+
+
+//            if(ownerModel.getId() != 0){
+//                res.setStatus(HttpServletResponse.SC_OK);
+//                if(ownerModel.getPassword().equals(owner.getPassword())) {
+//                    out.write("{\"message\": \"Login successfully\",\"page\":\""+ "owner" +"\"}");
+//                    System.out.println("Login successful");
+//                }else{
+//                    out.write("{\"message\": \"Wrong Password\"}");
+//                    System.out.println("Wrong password");
+//                }
+//            }else{
+//                res.setStatus(HttpServletResponse.SC_OK);
+//                out.write("{\"message\": \"Invalid Email\"}");
+//                System.out.println("Login incorrect");
+//            }
         }
         catch (Exception e) {
             e.printStackTrace();
